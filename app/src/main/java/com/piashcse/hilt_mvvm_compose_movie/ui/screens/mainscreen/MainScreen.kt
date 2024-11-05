c52f315efbf25831c08d93843bdd3630173d4852
Mehedi Hassan Piash <piash599@gmail.com>
2024-11-06 00:31:29 +0600

    - Fixing favorite icon visibility

--- app/src/main/java/com/piashcse/hilt_mvvm_compose_movie/ui/screens/mainscreen/MainScreen.kt
+++ app/src/main/java/com/piashcse/hilt_mvvm_compose_movie/ui/screens/mainscreen/MainScreen.kt
@@ -132,14 +132,20 @@ fun MainScreen() {
                 if (!isFavoriteActive.value) navController.navigate(Screen.FavoriteMovie.route)
                 isFavoriteActive.value = true
             }) {
-                if (currentRoute(navController) !== Screen.FavoriteMovie.route && currentRoute(
-                        navController
-                    ) !== Screen.FavoriteTvSeries.route
-                ) Icon(
-                    imageVector = Icons.Filled.Favorite,
-                    contentDescription = "Localized description",
-                    tint = Color.Gray
-                )
+                if (currentRoute(navController) !in listOf(
+                        Screen.FavoriteMovie.route,
+                        Screen.FavoriteTvSeries.route,
+                        Screen.MovieDetail.route,
+                        Screen.TvSeriesDetail.route,
+                        Screen.ArtistDetail.route
+                    )
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Favorite,
+                        contentDescription = "favorite",
+                        tint = Color.Gray
+                    )
+                }
             }
         })
     }, floatingActionButton = {
@@ -159,13 +165,15 @@ fun MainScreen() {
             }
         }
     }, bottomBar = {
-        when (currentRoute(navController)) {
-            Screen.NowPlaying.route, Screen.Popular.route, Screen.TopRated.route, Screen.Upcoming.route,
-            Screen.AiringTodayTvSeries.route, Screen.OnTheAirTvSeriesNav.route,
-            Screen.PopularTvSeries.route, Screen.TopRatedTvSeries.route,
-                -> {
-                BottomNavigationUI(navController, pagerState)
-            }
+        if (currentRoute(navController) !in listOf(
+                Screen.FavoriteMovie.route,
+                Screen.FavoriteTvSeries.route,
+                Screen.MovieDetail.route,
+                Screen.TvSeriesDetail.route,
+                Screen.ArtistDetail.route
+            )
+        ) {
+            BottomNavigationUI(navController, pagerState)
         }
     }, snackbarHost = {
         if (isConnected.not()) {
