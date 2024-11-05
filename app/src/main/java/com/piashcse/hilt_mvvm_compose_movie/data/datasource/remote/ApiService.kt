995ea02989df379293469bd43d0081a05def06eb
Mehedi Hassan Piash <piash599@gmail.com>
2024-11-05 17:17:47 +0600

    - Code refactor

--- app/src/main/java/com/piashcse/hilt_mvvm_compose_movie/data/datasource/remote/ApiService.kt
+++ app/src/main/java/com/piashcse/hilt_mvvm_compose_movie/data/datasource/remote/ApiService.kt
@@ -18,111 +18,111 @@ interface ApiService {
     suspend fun nowPlayingMovies(
         @Query("page") page: Int,
         @Query("with_genres") genreId: String?,
-        @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): BaseModelMovie
 
     @GET("movie/popular")
     suspend fun popularMovies(
         @Query("page") page: Int,
         @Query("with_genres") genreId: String?,
-        @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): BaseModelMovie
 
     @GET("movie/top_rated")
     suspend fun topRatedMovies(
         @Query("page") page: Int,
         @Query("with_genres") genreId: String?,
-        @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): BaseModelMovie
 
     @GET("movie/upcoming")
     suspend fun upcomingMovies(
         @Query("page") page: Int,
         @Query("with_genres") genreId: String?,
-        @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): BaseModelMovie
 
     @GET("movie/{movieId}")
     suspend fun movieDetail(
-        @Path("movieId") movieId: Int, @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Path("movieId") movieId: Int, @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): MovieDetail
 
     @GET("movie/{movieId}/recommendations")
     suspend fun recommendedMovie(
-        @Path("movieId") movieId: Int, @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Path("movieId") movieId: Int, @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): BaseModelMovie
 
     @GET("search/movie?page=1&include_adult=false")
     suspend fun searchMovie(
-        @Query("query") searchKey: String, @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Query("query") searchKey: String, @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): SearchBaseModel
 
     @GET("genre/movie/list")
-    suspend fun genreList(@Query("api_key") api_key: String = BuildConfig.API_KEY): Genres
+    suspend fun genreList(@Query("api_key") apiKey: String = BuildConfig.API_KEY): Genres
 
     @GET("discover/movie")
     suspend fun moviesByGenre(
         @Query("page") page: Int,
         @Query("with_genres") genreId: String,
-        @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): BaseModelMovie
 
     @GET("movie/{movieId}/credits")
     suspend fun movieCredit(
-        @Path("movieId") movieId: Int, @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Path("movieId") movieId: Int, @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): Artist
 
     @GET("person/{personId}")
     suspend fun artistDetail(
-        @Path("personId") personId: Int, @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Path("personId") personId: Int, @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): ArtistDetail
 
     @GET("tv/airing_today")
     suspend fun airingTodayTvSeries(
         @Query("page") page: Int,
         @Query("with_genres") genreId: String?,
-        @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): BaseModelTvSeries
 
     @GET("tv/on_the_air")
     suspend fun onTheAirTvSeries(
         @Query("page") page: Int,
         @Query("with_genres") genreId: String?,
-        @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): BaseModelTvSeries
 
     @GET("tv/popular")
     suspend fun popularTvSeries(
         @Query("page") page: Int,
         @Query("with_genres") genreId: String?,
-        @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): BaseModelTvSeries
 
     @GET("tv/top_rated")
     suspend fun topRatedTvSeries(
         @Query("page") page: Int,
         @Query("with_genres") genreId: String?,
-        @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): BaseModelTvSeries
 
     @GET("tv/{seriesId}")
     suspend fun tvSeriesDetail(
-        @Path("seriesId") seriesId: Int, @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Path("seriesId") seriesId: Int, @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): TvSeriesDetail
 
     @GET("tv/{seriesId}/recommendations")
     suspend fun recommendedTvSeries(
-        @Path("seriesId") seriesId: Int, @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Path("seriesId") seriesId: Int, @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): BaseModelTvSeries
 
     @GET("tv/{seriesId}/credits")
     suspend fun tvSeriesCredit(
-        @Path("seriesId") seriesId: Int, @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Path("seriesId") seriesId: Int, @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): Artist
 
     @GET("search/tv?page=1&include_adult=false")
     suspend fun searchTvSeries(
         @Query("query") searchKey: String,
-        @Query("api_key") api_key: String = BuildConfig.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): SearchBaseModel
 }
