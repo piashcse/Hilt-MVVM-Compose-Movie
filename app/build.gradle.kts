8ac7702973633edc1f564b169b7c135c592cf1c1
Mehedi Hassan Piash <piash599@gmail.com>
2025-12-12 21:11:42 +0600

    - Update navigation 3 - Code optimization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.piashcse.hilt_mvvm_compose_movie"
         minSdk = 23
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 40
         versionName = "2.2.0"
 
@@ -71,8 +71,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.material.icons.extended)
-    implementation(libs.androidx.navigation.runtime.ktx)
-    implementation(libs.androidx.navigation.compose)
+
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.navigation3.ui)
 
